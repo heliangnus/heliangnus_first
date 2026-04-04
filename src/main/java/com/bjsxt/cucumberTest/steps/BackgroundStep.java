acebc7df47f7efc30e0b74e717df47d80a4f37e8
heliangnus <heliangnus@126.com>
2026-04-04 23:46:12 +0800

    file has been111 changed for only one

--- src/main/java/com/bjsxt/cucumberTest/steps/BackgroundStep.java
+++ src/main/java/com/bjsxt/cucumberTest/steps/BackgroundStep.java
@@ -40,7 +40,6 @@ public class BackgroundStep {
 public void testv(){
     System.out.println("new addedddd ");
     System.out.println("new addedddd ");
-    System.out.println("new ddddd ");
     System.out.println("new addedddd ");
 
 }
